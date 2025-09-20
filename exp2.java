d11f71e68c3473c0212068245210fca17b1b591b
Vishal Kumar <vishalcool9341@gmail.com>
2025-09-20 19:06:02 +0530

    Add files via upload

---
+++ exp2.java
@@ -0,0 +1,18 @@
+public class exp2 {
+    public static int Sum (int a, int b){
+      return a+b;
+    }
+    public static int Subs (int a, int b){
+      return a-b;
+    }
+        public static int Multiply (int a, int b){
+      return a*b;
+    }
+          public static int divide (int a, int b){
+      return a/b;
+    }
+public static void main(String args[]){
+    int f=Sum(4,6);
+    System.out.println("Final Answer is: "+f);
+}
+}
